66edae02a5042fe2a6450ac6dba36134be500fd0
franklinjr12 <franklinjunior12@hotmail.com>
2017-09-18 10:57:28 -0300

    testando header urn

--- Database/main.cpp
+++ Database/main.cpp
@@ -21,14 +21,18 @@ int main(){
     UrnElection* ue1 = new UrnElection(mc1, m1);
     UrnSetup* us1 = new UrnSetup(mc1, m1);
     us1->newElection();
-    us1->addCandidate("adnaldo",7);
-    us1->addCandidate("oie", 3);
+
+    string cand1 = "Agnaldo";
+    string cand2 = "oie";
+
+    us1->addCandidate(&cand1[0], cand1.size());
+    us1->addCandidate(&cand2[0], cand2.size());
 
 
    char* a = ue1->listCandidates();
-    //for(int i = 0; i < 10; i++)
-    //    cout << a[i];
-    //cout << endl;
+    for(int i = 0; i < 10; i++)
+        cout << a[i];
+    cout << endl;
 
     return 0;
 }
