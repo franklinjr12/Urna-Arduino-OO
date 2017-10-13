479406570b19a701b2dffbce060ba792438fc16a
AirtonRamos <airtonramos@alunos.utfpr.edu.br>
2017-10-13 17:05:17 -0300

    cont

--- CORE/CORE.cpp
+++ CORE/CORE.cpp
@@ -6,8 +6,6 @@
 #include "interfaceGrafica.h"
 #include "keyBoard"
 
-
-
 using namespace std;
 
 class CORE {
@@ -25,6 +23,7 @@ void CORE::memoryCore(){
 
     char vote[nomeSize1]="Candidator1";
 
+//================================================================================
 //        VERIFICA SE O VOTO DE CERTO OU NÃO!!!
     if(!UrnElection::vote(&vote[0], nomeSize1)){
 //        LCD PRINT OK (VOTO DEU CERTO)
@@ -38,10 +37,14 @@ void CORE::memoryCore(){
         InterfaceGrafica::mostrarMensagem(????, ????, espera, &msg2[0]);
 //         ESTOU COM DUVIDAS QUANTO A "LINHAS" E "COLUNA" QUE ESTA FUNÇÃO RECEBE
     }
+//==============================================================================
 
+
+//==============================================================================
     char listCandidatos[]= UrnElection::listCandidates();
 //    FAZER O LCD IMPRIMIR A LISTA DE  CANDIDATOS
     InterfaceGrafica::mostrarMensagem(????, ????, espera, &listCandidatos[0]);
+//==============================================================================
 }
 
 void CORE::lcdCore(){
