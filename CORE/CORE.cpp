78b6c53d262f4214af4c51d49b2e0b9c8c100bb4
AirtonRamos <airtonramos@alunos.utfpr.edu.br>
2017-10-13 14:54:18 -0300

    memory control
    Controle de dados da memória

---
+++ CORE/CORE.cpp
@@ -0,0 +1,34 @@
+//TRATAMENTO DE DADOS
+
+#include <Urn.h>
+#include <iostream>
+
+
+using namespace std;
+
+class CORE {
+public:
+	void memoryCore();
+	void boardCore();
+	void lcdCore();
+};
+
+
+void CORE::memoryCore(){
+    int nomeSize1=99;
+    char vote[nomeSize1];
+    char listCandidatos[]= UrnElection::listCandidates();
+
+
+}
+
+void CORE::lcdCore(){
+
+
+}
+
+void CORE::boardCore(){
+
+
+
+}
