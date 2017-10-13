aae55c9f4fc37327cc9a2649dcb99229f2989018
AirtonRamos <airtonramos@alunos.utfpr.edu.br>
2017-10-13 16:55:59 -0300

    memory data
    Invocação dos dados da memória!

--- CORE/CORE.cpp
+++ CORE/CORE.cpp
@@ -21,17 +21,27 @@ public:
 void CORE::memoryCore(){
     char nomeSize1=99;
 
+    int espera=1000;
+
     char vote[nomeSize1]="Candidator1";
 
+//        VERIFICA SE O VOTO DE CERTO OU NÃO!!!
     if(!UrnElection::vote(&vote[0], nomeSize1)){
-        //LCD PRINT OK (VOTO DEU CERTO)
-        //IMPLEMENTAR PARA O LCD IMPRIMIR
+//        LCD PRINT OK (VOTO DEU CERTO)
+//        IMPLEMENTAR PARA O LCD IMPRIMIR
+        char msg1[]="SUCCESS!!!"
+        InterfaceGrafica::mostrarMensagem(????, ????, espera, &msg1[0]);
+//        ESTOU COM DUVIDAS QUANTO A "LINHAS" E "COLUNA" QUE ESTA FUNÇÃO RECEBE
     }else{
-        // LCD PRINT ERRO (O VOTO NÃO DEU CERTO)
+//         LCD PRINT ERRO (O VOTO NÃO DEU CERTO)
+        char msg2[]="ERROR!!!"
+        InterfaceGrafica::mostrarMensagem(????, ????, espera, &msg2[0]);
+//         ESTOU COM DUVIDAS QUANTO A "LINHAS" E "COLUNA" QUE ESTA FUNÇÃO RECEBE
     }
 
     char listCandidatos[]= UrnElection::listCandidates();
 //    FAZER O LCD IMPRIMIR A LISTA DE  CANDIDATOS
+    InterfaceGrafica::mostrarMensagem(????, ????, espera, &listCandidatos[0]);
 }
 
 void CORE::lcdCore(){
