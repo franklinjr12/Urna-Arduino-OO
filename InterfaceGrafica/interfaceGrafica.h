84a784b9fb9d48b6b68f983922ca42d3cafacedc
Lucas de Oliveira Pereira <olucas1998@gmail.com>
2017-10-16 10:48:23 -0200

    dump file

--- InterfaceGrafica/interfaceGrafica.h
+++
@@ -1,7 +0,0 @@
-/* Desenvolvido por Lucas Oliveira */
-/* CLASSE INTERFACE GRAFICA */
-class InterfaceGrafica{
-  public:
-    void mostrarMensagem(int linha, int coluna, int delay, char* mensagem);
-};
-/* FIM CLASSE INTERFACE GRAFICA */
