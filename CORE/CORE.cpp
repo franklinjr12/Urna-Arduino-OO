452c709e995cdae47da00a11351a8d7c3276a954
AirtonRamos <airtonramos@alunos.utfpr.edu.br>
2017-10-19 19:11:43 -0200

    core

--- CORE/CORE.cpp
+++ CORE/CORE.cpp
@@ -1,41 +1,35 @@
+#include "CORE.h"
+
 //TRATAMENTO DE DADOS
 
 #include <iostream>
 #include <string>
 #include "Urn.h"
-#include "interfaceGrafica.h"
-
-//#include "keyBoard"
-//FALTA A IMPLEMENTA플O DO KEYBOARD
-
 
 using namespace std;
 
-class CORE {
-public:
-	void memoryCore();
-	void boardCore();
-	void lcdCore();
-};
 //==============================================================================
 //EXIBE MENSAGEM DE BEM VINDO
-void CORE::lcdCore(){
-    int espera=1000;
-    char mensagemI[]="Bem Vindo a Urna OO";
-    InterfaceGrafica::mostrarMensagem(????, ????, espera, &mensagemI[0]);
-}
+
+//ESSA PARTE SER� IMPLEMENTADA NA IDE DO ARDUINO
+//void CORE::lcdCore(){
+//    int espera=1000;
+//    char mensagemI[]="Bem Vindo a Urna OO";
+//    InterfaceGrafica::mostrarMensagem(????, ????, espera, &mensagemI[0]);
+//}
 //==============================================================================
 
 
 //==============================================================================
 //IMPLEMENTCAO PARA O TECLADO
-void CORE::boardCore(){
-
-//CODE
-
-}
+//void CORE::boardCore(){
+//
+////CODE
+//
+//}
 //==============================================================================
 
+//OBS: TODA PARTE ENVOLVENDO TECLADO E DISPLAY SER� IMPLEMENTADA NO ARDUINO
 
 void CORE::memoryCore(){
     char nomeSize1=99;
@@ -48,14 +42,15 @@ void CORE::memoryCore(){
 //        VERIFICA SE O VOTO DE CERTO OU NAO!!!
     if(!UrnElection::vote(&vote[0], nomeSize1)){
 //        LCD PRINT OK (VOTO DEU CERTO)
-//        IMPLEMENTAR PARA O LCD IMPRIMIR
-        char msg1[]="SUCCESS!!!"
-        InterfaceGrafica::mostrarMensagem(????, ????, espera, &msg1[0]);
+
+        char msg1[]="SUCCESS!!!";
+
+//        InterfaceGrafica::mostrarMensagem(????, ????, espera, &msg1[0]);
 //        ESTOU COM DUVIDAS QUANTO A "LINHAS" E "COLUNA" QUE ESTA FUNCAO RECEBE
     }else{
 //         LCD PRINT ERRO (O VOTO NAO DEU CERTO)
         char msg2[]="ERROR!!!"
-        InterfaceGrafica::mostrarMensagem(????, ????, espera, &msg2[0]);
+//         InterfaceGrafica::mostrarMensagem(????, ????, espera, &msg2[0]);
 //         ESTOU COM DUVIDAS QUANTO A "LINHAS" E "COLUNA" QUE ESTA FUN플O RECEBE
     }
 //==============================================================================
@@ -64,7 +59,7 @@ void CORE::memoryCore(){
 //==============================================================================
     char listCandidatos[]= UrnElection::listCandidates();
 //    FAZER O LCD IMPRIMIR A LISTA DE  CANDIDATOS
-    InterfaceGrafica::mostrarMensagem(????, ????, espera, &listCandidatos[0]);
+//    InterfaceGrafica::mostrarMensagem(????, ????, espera, &listCandidatos[0]);
 //==============================================================================
 }
 
